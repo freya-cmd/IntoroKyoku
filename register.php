52797a0dfa5bff6c805c5dd7d311d799f8eb43cd
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-28 11:03:47 -0400

    clean data

--- register.php
+++ register.php
@@ -1,6 +1,24 @@
 <!DOCTYPE html>
 <?php
 
+function sanitizeFormPassword($inputText) {
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    return $inputText;
+}
+function sanitizeFormString($inputText) {
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+
+}
+
 if(isset($_POST['loginButton'])) {
     //Login Button has been clicked
 
@@ -8,7 +26,16 @@ if(isset($_POST['loginButton'])) {
 
 if(isset($_POST['registerButton'])) {
     //Register Button has been clicked
-    $username
+    $username = sanitizeFormUsername($_POST['username']);
+    $firstName = sanitizeFormString($_POST['firstName']);
+    $lastName = sanitizeFormString($_POST['lastName']);
+    $email = sanitizeFormString($_POST['email']);
+    $email2 = sanitizeFormString($_POST['email2']);
+    $password = sanitizeFormPassword($_POST['password']);
+    $password2 = sanitizeFormPassword($_POST['password2']);
+
+
+
 
     
 
