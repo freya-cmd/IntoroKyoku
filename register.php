cdf760f6cb7aa886704eb815384544e2bf242391
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-28 10:03:30 -0400

    register

--- register.php
+++ register.php
@@ -7,6 +7,67 @@
     <title>Audify Welcome</title>
 </head>
 <body>
-    This is the Register Page.
+    <div id="inputContainer">
+        <form id='loginForm' action="register.php" method="POST">
+        <h2>Log in to your account</h2>
+
+        <p>
+        <label for="loginUsername">Username</label>
+        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+        </p>
+
+        <p>
+        <label for="loginPassword">Password</label>
+        <input id="loginPassword" name="loginPassword" type="password" required>
+        </p>
+
+        <button type="submit" name="loginButton">Login</button>
+        </form>
+
+
+        <form id='registerForm' action="register.php" method="POST">
+        <h2>Create your account</h2>
+
+        <p>
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+        </p>
+
+        <p>
+        <label for="firstName">First Name</label>
+        <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+        </p>
+
+        <p>
+        <label for="lastName">Last Name</label>
+        <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+        </p>
+
+        <p>
+        <label for="email">E-mail</label>
+        <input id="email" name="email" type="email" placeholder="e.g. plums@hotmail.com" required>
+        </p>
+
+        <p>
+        <label for="email2">Confirm E-mail</label>
+        <input id="email2" name="email2" type="email" placeholder="e.g. plums@hotmail.com" required>
+        </p>
+
+        <p>
+        <label for="loginPassword">Password</label>
+        <input id="password" name="password" type="password" required>
+        </p>
+
+        <p>
+        <label for="loginPassword">Password</label>
+        <input id="password" name="password" type="password" required>
+        </p>
+
+        <button type="submit" name="loginButton">Login</button>
+        </form>
+
+    </div>
+
+
 </body>
 </html>
