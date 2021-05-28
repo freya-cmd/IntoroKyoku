739fef1c13edcb0b4c6eff3ecfc062b69825ff5e
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-05-28 14:06:24 -0400

    more validation

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -29,14 +29,32 @@ class Account {
     
     private function validateFirstname($fn) {
     
+
+        if(strlen($fn) > 25 || strlen($fn) < 2) {
+            array_push($this->errorArray, "Your first name must be between 5 and 25 characters");
+            return;
+        }
     }
     
     private function validateLastname($ln) {
     
+        if(strlen($ln) > 25 || strlen($ln) < 2) {
+            array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+            return;
+        }
     }
     
     private function validateEmails($em, $em2) {
-    
+        if($em != em2) {
+            array_push($this->errorArray, "Your emails do not match");
+            return;
+        }
+        if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, "Email is invalid");
+            return;
+        }
+
+        //TODO: check username is available
     }
     
     private function validatePasswords($pw, $pw2) {
