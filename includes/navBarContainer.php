aaf2f6288e6cef4626742e1ce4a7b3bc547fa5ba
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-06-01 12:38:55 -0400

    add navbar php

---
+++ includes/navBarContainer.php
@@ -0,0 +1,36 @@
+<div id="navBarContainer">
+<nav class="navBar">
+
+<a href="index.php" class="logo">
+    <img src="assets/images/home.png" alt="home">
+    </a>
+
+    <div class="group">
+        <div class="navItem"> 
+            <a href="search.php" class="navItemLink">Search
+                <img src="assets/images/search.png" class="icon" alt="Search">
+            </a>
+        </div>
+
+    </div>
+
+    <div class="group">
+    
+    <div class="navItem"> 
+            <a href="browse.php" class="navItemLink">Browse</a>
+        </div>
+
+        <div class="navItem"> 
+            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+        </div>
+
+        <div class="navItem"> 
+            <a href="profile.php" class="navItemLink">Profile</a>
+        </div>
+
+
+        </div>
+
+
+</nav>
+</div>
