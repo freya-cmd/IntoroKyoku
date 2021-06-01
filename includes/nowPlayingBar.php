b9b44325eb2307416b580465f273b426085ad70c
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-06-01 16:30:48 -0400

    organize bars

---
+++ includes/nowPlayingBar.php
@@ -0,0 +1,104 @@
+<div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+
+
+        <div id="nowPlayingLeft">
+
+            <div class="content">
+                <span class="albumLink">
+                <img src="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png" class ="albumArtwork">
+                </span>
+
+                <div class="trackInfo">
+
+                    <span class="trackName">
+                        <span>Track Name!</span>
+                    </span>
+
+                    <span class="artistName">
+                        <span>Artist Name</span>
+                    </span>
+
+                    </div>
+
+
+            </div>
+
+        </div>
+
+    <div id="nowPlayingCenter">
+
+        <div class="content playerControl">
+
+            <div class="buttons">
+
+                <button class="controlButton shuffle" title="Shuffle Button">
+                        <img src="assets/images/shuffle_dark.png" alt="shuffle">
+                </button>
+
+                <button class="controlButton previous" title="Previous Button">
+                        <img src="assets/images/rewind_dark.png" alt="Previous">
+                </button>
+
+                <button class="controlButton play" title="Play Button">
+                        <img src="assets/images/play_dark.png" alt="Play">
+                </button>
+
+                <button class="controlButton pause" title="Pause Button" style="display: none;">
+                        <img src="assets/images/pause_dark.png" alt="Pause">
+                </button>
+
+                <button class="controlButton next" title="Next Button">
+                        <img src="assets/images/forward_dark.png" alt="Next">
+                </button>
+
+                <button class="controlButton repeat" title="Repeat Button">
+                        <img src="assets/images/repeat.png" alt="Repeat">
+                </button>
+
+
+            </div>
+
+        <div class="playbackBar">
+
+        <span class="progressTime current">0.00</span>
+            <div class="progressBar">
+                <div class="progressBarBg">
+                    <div class="progress"></div>
+                    </div>
+                </div>
+
+
+        <span class="progressTime remaining">0.00</span>
+
+            </div>
+
+        </div>
+
+
+        </div>
+
+    <div id="nowPlayingRight">
+
+    <div class="volumeBar">
+
+        <button class="controlButton volume" title="Volume Button">
+            <img src="assets/images/volume.png" alt="Volume">
+
+        </button>
+
+        <div class="progressBar">
+                <div class="progressBarBg">
+                    <div class="progress"></div>
+                    </div>
+                </div>
+
+</div>
+
+        </div>
+
+
+
+
+    </div>
+</div>
