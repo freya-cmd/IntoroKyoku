783889efb3cd247deaf719a618320523301f7d7a
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-06-01 11:06:27 -0400

    add navbar changes

--- index.php
+++ index.php
@@ -39,7 +39,9 @@
 
                             <div class="group">
                                 <div class="navItem"> 
-                                    <a href="search.php" class="navItemLink">Search</a>
+                                    <a href="search.php" class="navItemLink">Search
+                                        <img src="assets/images/search.png" class="icon" alt="Search">
+                                    </a>
                                 </div>
 
                             </div>
