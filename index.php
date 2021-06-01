d3a85ede9c547afae88f8b683cbb8195de66f53b
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-06-01 09:37:46 -0400

    playbar assets

--- index.php
+++ index.php
@@ -28,6 +28,70 @@
 <div id="nowPlayingBarContainer">
     <div id="nowPlayingBar">
 
+    <div id="nowPlayingLeft">
+
+        </div>
+
+    <div id="nowPlayingCenter">
+
+        <div class="content playerControl">
+
+            <div class="buttons">
+
+                <button class="controlButton shuffle" title="Shuffle Button">
+                        <img src="assets/images/shuffle_dark.png" alt="shuffle">
+                </button>
+
+                <button class="controlButton previous" title="Previous Button">
+                        <img src="assets/images/rewind_dark.png" alt="Previous">
+                </button>
+
+                <button class="controlButton play" title="Play Button">
+                        <img src="assets/images/play_dark.png" alt="Play">
+                </button>
+
+                <button class="controlButton pause" title="Pause Button" style="display: none;">
+                        <img src="assets/images/pause_dark.png" alt="Pause">
+                </button>
+
+                <button class="controlButton next" title="Next Button">
+                        <img src="assets/images/forward_dark.png" alt="Next">
+                </button>
+
+                <button class="controlButton repeat" title="Repeat Button">
+                        <img src="assets/images/repeat.png" alt="Repeat">
+                </button>
+
+
+            </div>
+
+        <div class="playbackBar">
+
+        <span class="progressTime current">0.00</span>
+            <div class="progressBar">
+                <div class="progressBarBg">
+                    <div class="progress">
+
+                        </div>
+                    </div>
+
+                </div>
+        <span class="progressTime remaining">0.00</span>
+
+            </div>
+
+        </div>
+
+
+        </div>
+
+    <div id="nowPlayingRight">
+
+        </div>
+
+
+
+
     </div>
 </div>
 
