3f57fe26df59c174ba5e2189c7efab196f400021
Nathaniel Betancourt <nathanielbetancourt@hotmail.com>
2021-06-01 10:42:37 -0400

    add nav items

--- index.php
+++ index.php
@@ -25,7 +25,48 @@
 
 <body>
 
-<div id="mainContainer">
+    <div id="mainContainer">
+
+
+        <div id="topContainer">
+
+            <div id="navBarContainer">
+                    <nav class="navBar">
+
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/home.png" alt="home">
+                            </a>
+
+                            <div class="group">
+                                <div class="navItem"> 
+                                    <a href="search.php" class="navItemLink">Search</a>
+                                </div>
+
+                            </div>
+
+                            <div class="group">
+                            
+                            <div class="navItem"> 
+                                    <a href="browse.php" class="navItemLink">Browse</a>
+                                </div>
+
+                                <div class="navItem"> 
+                                    <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                                </div>
+
+                                <div class="navItem"> 
+                                    <a href="profile.php" class="navItemLink">Profile</a>
+                                </div>
+
+
+                                </div>
+
+
+                        </nav>
+                </div>
+
+            </div>
+
 
 <div id="nowPlayingBarContainer">
     <div id="nowPlayingBar">
